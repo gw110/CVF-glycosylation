0be763945deaa8bd42635a4d597b7e384e42ed1a
gang wu <fromgangwu@gmail.com>
2022-05-04 17:29:45 +0100

    Figure 6 top panel

--- R script.R
+++ R script.R
@@ -3,48 +3,36 @@
 ## Load ggplot2 
 library(ggplot2)
 
-############# Figure 5 lower panel ##########################
+
+############# Figure 6 upper panel, Non-fucosylation % #################
 
 ## load the data
-res=read.csv('Figure_5_lower_panel_data.csv')
+res=read.csv('Figure_6_upper_panel_non_fucosylation_data.csv')
+
+## replace NA values with 0
 res[is.na(res)]=0
 
 ## Non-fucosylation %
-ggplot(data=subset.data.frame(res, dHex.count==0),
-       aes(Pregnancy_status, (dHex_Intensity/Total_Intensity)*100))+
+ggplot(data=subset.data.frame(res, dHex.count==0 &
+                                Outcome!='Non-pregnant'),
+       aes(Outcome, (dHex_Intensity/Total_Intensity)*100))+
   geom_boxplot(outlier.alpha = 0)+
   geom_point(position=position_jitter(width = 0.3),shape=2)+
   geom_vline(xintercept=1.5+c(0:7), linetype='dotted')+
-  labs(y='Glycan proportion %')
+  labs(y='Non-fucosylation %')
 
-## Mono-fucosylation %
-ggplot(data=subset.data.frame(res, dHex.count==1),
-       aes(Pregnancy_status, (dHex_Intensity/Total_Intensity)*100))+
-  geom_boxplot(outlier.alpha = 0)+
-  geom_point(position=position_jitter(width = 0.3),shape=2)+
-  geom_vline(xintercept=1.5+c(0:7), linetype='dotted')+
-  labs(y='Glycan proportion %')
+############# Figure 6 upper panel, Sialylation % #################
 
-## Bi-fucosylation %
-ggplot(data=subset.data.frame(res, dHex.count==2),
-       aes(Pregnancy_status, (dHex_Intensity/Total_Intensity)*100))+
-  geom_boxplot(outlier.alpha = 0)+
-  geom_point(position=position_jitter(width = 0.3),shape=2)+
-  geom_vline(xintercept=1.5+c(0:7), linetype='dotted')+
-  labs(y='Glycan proportion %')
+## load the data
+res=read.csv('Figure_6_upper_panel_sialylation_data.csv')
 
-## Tri-fucosylation %
-ggplot(data=subset.data.frame(res, dHex.count==3),
-       aes(Pregnancy_status, (dHex_Intensity/Total_Intensity)*100))+
-  geom_boxplot(outlier.alpha = 0)+
-  geom_point(position=position_jitter(width = 0.3),shape=2)+
-  geom_vline(xintercept=1.5+c(0:7), linetype='dotted')+
-  labs(y='Glycan proportion %')
+## replace NA values with 0
+res[is.na(res)]=0
 
-## Tetra-fucosylation %
-ggplot(data=subset.data.frame(res, dHex.count==4),
-       aes(Pregnancy_status, (dHex_Intensity/Total_Intensity)*100))+
+## Sialylation %
+ggplot(data=subset.data.frame(res, Outcome!='Non-pregnant'),
+       aes(Outcome, (NeuAc_Intensity/Total_Intensity)*100))+
   geom_boxplot(outlier.alpha = 0)+
   geom_point(position=position_jitter(width = 0.3),shape=2)+
   geom_vline(xintercept=1.5+c(0:7), linetype='dotted')+
-  labs(y='Glycan proportion %')
+  labs(y='Sialylation %')
