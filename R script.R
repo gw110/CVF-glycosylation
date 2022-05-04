885cf94d00104479fbb9ad81fd917f15c08ddba7
gang wu <fromgangwu@gmail.com>
2022-05-04 17:37:04 +0100

    Figure 7 top panel

--- R script.R
+++ R script.R
@@ -3,10 +3,10 @@
 ## Load ggplot2 
 library(ggplot2)
 
-############# Figure 6 lower panel, Non-fucosylation % #################
+############# Figure 7 upper panel #################
 
 ## load the data
-res=read.csv('Figure_6_lower_panel_non_fucosylation_data.csv')
+res=read.csv('Figure_7_upper_panel_data.csv')
 
 ## replace NA values with 0
 res[is.na(res)]=0
@@ -14,24 +14,46 @@ res[is.na(res)]=0
 ## Non-fucosylation %
 ggplot(data=subset.data.frame(res, dHex.count==0 &
                                 Outcome!='Non-pregnant'),
-       aes(Outcome, (dHex_Intensity/Total_Intensity)*100))+
+       aes(CST, (dHex_Intensity/Total_Intensity)*100))+
   geom_boxplot(outlier.alpha = 0)+
   geom_point(position=position_jitter(width = 0.3),shape=2)+
   geom_vline(xintercept=1.5+c(0:7), linetype='dotted')+
   labs(y='Non-fucosylation %')
 
-############# Figure 6 lower panel, Sialylation % #################
-
-## load the data
-res=read.csv('Figure_6_lower_panel_sialylation_data.csv')
-
-## replace NA values with 0
-res[is.na(res)]=0
-
-## Sialylation %
-ggplot(data=subset.data.frame(res, Outcome!='Non-pregnant'),
-       aes(Outcome, (NeuAc_Intensity/Total_Intensity)*100))+
+## Mono-fucosylation %
+ggplot(data=subset.data.frame(res, dHex.count==1 &
+                                Outcome!='Non-pregnant'),
+       aes(CST, (dHex_Intensity/Total_Intensity)*100))+
   geom_boxplot(outlier.alpha = 0)+
   geom_point(position=position_jitter(width = 0.3),shape=2)+
   geom_vline(xintercept=1.5+c(0:7), linetype='dotted')+
-  labs(y='Sialylation %')
+  labs(y='Mono-fucosylation %')
+
+
+## Bi-fucosylation % 
+ggplot(data=subset.data.frame(res, dHex.count==2 &
+                                Outcome!='Non-pregnant'),
+       aes(CST, (dHex_Intensity/Total_Intensity)*100))+
+  geom_boxplot(outlier.alpha = 0)+
+  geom_point(position=position_jitter(width = 0.3),shape=2)+
+  geom_vline(xintercept=1.5+c(0:7), linetype='dotted')+
+  labs(y='Bi-fucosylation %')
+
+
+## Tri-fucosylation % 
+ggplot(data=subset.data.frame(res, dHex.count==3 &
+                                Outcome!='Non-pregnant'),
+       aes(CST, (dHex_Intensity/Total_Intensity)*100))+
+  geom_boxplot(outlier.alpha = 0)+
+  geom_point(position=position_jitter(width = 0.3),shape=2)+
+  geom_vline(xintercept=1.5+c(0:7), linetype='dotted')+
+  labs(y='Tri-fucosylation %')
+
+## Tetra-fucosylation % 
+ggplot(data=subset.data.frame(res, dHex.count==4 &
+                                Outcome!='Non-pregnant'),
+       aes(CST, (dHex_Intensity/Total_Intensity)*100))+
+  geom_boxplot(outlier.alpha = 0)+
+  geom_point(position=position_jitter(width = 0.3),shape=2)+
+  geom_vline(xintercept=1.5+c(0:7), linetype='dotted')+
+  labs(y='Tetra-fucosylation %')
