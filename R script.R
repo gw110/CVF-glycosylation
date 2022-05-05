7943a52c614017b0f59a6a72cb7a251e83a522a4
gang wu <fromgangwu@gmail.com>
2022-05-05 09:44:41 +0100

    Figure 9

--- R script.R
+++ R script.R
@@ -3,47 +3,139 @@
 ## Load ggplot2 
 library(ggplot2)
 
-############# Figure 8 lower panel #################
 
-## load the data
-res=read.csv('Figure_8_lower_panel_data.csv')
+############# Figure 9 A ####################################
 
-## replace NA values with 0
-res[is.na(res)]=0
+## import the data
+res=read.csv('Figure_9_A.csv')
 
-## Glycans without fucosylation
-ggplot(data=subset.data.frame(res, dHex.count==0 &
-                                Outcome!='Non-pregnant'),
-       aes(CST, (NeuAc_Intensity/Total_Intensity)*100))+
-  geom_boxplot(outlier.alpha = 0)+
-  geom_point(position=position_jitter(width = 0.3),shape=2)+
-  geom_vline(xintercept=1.5+c(0:7), linetype='dotted')+
-  labs(y='Sialylation %')
+## add relative intensity column
+res['relative.intensity']=res$dHex_Intensity/res$total_Intensity
 
-## Glycans with mono-fucosylation
-ggplot(data=subset.data.frame(res, dHex.count==1 &
-                                Outcome!='Non-pregnant'),
-       aes(CST, (NeuAc_Intensity/Total_Intensity)*100))+
-  geom_boxplot(outlier.alpha = 0)+
-  geom_point(position=position_jitter(width = 0.3),shape=2)+
-  geom_vline(xintercept=1.5+c(0:7), linetype='dotted')+
-  labs(y='Sialylation %')
+## plot the results IL-1 beta
+temp=cor.test(res$relative.intensity, log2(as.numeric(res$IL1.beta)))
+coefficient=as.vector(temp[[4]])
+coefficient=round(coefficient,digits = 2)
+p=temp[[3]]
+p=round(p, digits = 4)
+ggplot(res, aes(relative.intensity*100, log2(as.numeric(IL1.beta))))+
+  geom_point()+
+  labs(x='Glycan proportion %', y='log2(IL-1 beta)',
+       subtitle = paste('coefficient = ',coefficient,', ', 'p = ',p,sep=''))+
+  geom_smooth(method=lm)+
+  theme( plot.subtitle = element_text(hjust = 0.5, face = 'italic'))
 
-## Glycans with bi-fucosylation
-ggplot(data=subset.data.frame(res, dHex.count==2 &
-                                Outcome!='Non-pregnant'),
-       aes(CST, (NeuAc_Intensity/Total_Intensity)*100))+
-  geom_boxplot(outlier.alpha = 0)+
-  geom_point(position=position_jitter(width = 0.3),shape=2)+
-  geom_vline(xintercept=1.5+c(0:7), linetype='dotted')+
-  labs(y='Sialylation %')
+## plot the results IL-18
+temp=cor.test(res$relative.intensity, log2(as.numeric(res$IL.18)))
+coefficient=as.vector(temp[[4]])
+coefficient=round(coefficient,digits = 2)
+p=temp[[3]]
+p=round(p, digits = 4)
+ggplot(res, aes(relative.intensity*100, log2(as.numeric(IL.18))))+
+  geom_point()+
+  labs(x='Glycan proportion %', y='log2(IL-1 18)',
+       subtitle = paste('coefficient = ',coefficient,', ', 'p = ',p,sep=''))+
+  geom_smooth(method=lm)+
+  theme( plot.subtitle = element_text(hjust = 0.5, face = 'italic'))
 
-## Glycans with tri-fucosylation
-ggplot(data=subset.data.frame(res, dHex.count==3 &
-                                Outcome!='Non-pregnant'),
-       aes(CST, (NeuAc_Intensity/Total_Intensity)*100))+
-  geom_boxplot(outlier.alpha = 0)+
-  geom_point(position=position_jitter(width = 0.3),shape=2)+
-  geom_vline(xintercept=1.5+c(0:7), linetype='dotted')+
-  labs(y='Sialylation %')
+############# Figure 9 B ####################################
 
+## import the data
+res=read.csv('Figure_9_B.csv')
+
+## add relative intensity column
+res['relative.intensity']=res$dHex_Intensity/res$total_Intensity
+
+## plot the results IL-1 beta
+temp=cor.test(res$relative.intensity, log2(as.numeric(res$IL1.beta)))
+coefficient=as.vector(temp[[4]])
+coefficient=round(coefficient,digits = 2)
+p=temp[[3]]
+p=round(p, digits = 4)
+ggplot(res, aes(relative.intensity*100, log2(as.numeric(IL1.beta))))+
+  geom_point()+
+  labs(x='Glycan proportion %', y='log2(IL-1 beta)',
+       subtitle = paste('coefficient = ',coefficient,', ', 'p = ',p,sep=''))+
+  geom_smooth(method=lm)+
+  theme( plot.subtitle = element_text(hjust = 0.5, face = 'italic'))
+
+## plot the results IL-18
+temp=cor.test(res$relative.intensity, log2(as.numeric(res$IL.18)))
+coefficient=as.vector(temp[[4]])
+coefficient=round(coefficient,digits = 2)
+p=temp[[3]]
+p=round(p, digits = 4)
+ggplot(res, aes(relative.intensity*100, log2(as.numeric(IL.18))))+
+  geom_point()+
+  labs(x='Glycan proportion %', y='log2(IL-1 18)',
+       subtitle = paste('coefficient = ',coefficient,', ', 'p = ',p,sep=''))+
+  geom_smooth(method=lm)+
+  theme( plot.subtitle = element_text(hjust = 0.5, face = 'italic'))
+
+############# Figure 9 C ####################################
+
+## import the data
+res=read.csv('Figure_9_C.csv')
+
+## add relative intensity column
+res['relative.intensity']=res$dHex_Intensity/res$total_Intensity
+
+## plot the results IL-1 beta
+temp=cor.test(res$relative.intensity, log2(as.numeric(res$IL1.beta)))
+coefficient=as.vector(temp[[4]])
+coefficient=round(coefficient,digits = 2)
+p=temp[[3]]
+p=round(p, digits = 4)
+ggplot(res, aes(relative.intensity*100, log2(as.numeric(IL1.beta))))+
+  geom_point()+
+  labs(x='Glycan proportion %', y='log2(IL-1 beta)',
+       subtitle = paste('coefficient = ',coefficient,', ', 'p = ',p,sep=''))+
+  geom_smooth(method=lm)+
+  theme( plot.subtitle = element_text(hjust = 0.5, face = 'italic'))
+
+## plot the results IL-18
+temp=cor.test(res$relative.intensity, log2(as.numeric(res$IL.18)))
+coefficient=as.vector(temp[[4]])
+coefficient=round(coefficient,digits = 2)
+p=temp[[3]]
+p=round(p, digits = 4)
+ggplot(res, aes(relative.intensity*100, log2(as.numeric(IL.18))))+
+  geom_point()+
+  labs(x='Glycan proportion %', y='log2(IL-1 18)',
+       subtitle = paste('coefficient = ',coefficient,', ', 'p = ',p,sep=''))+
+  geom_smooth(method=lm)+
+  theme( plot.subtitle = element_text(hjust = 0.5, face = 'italic'))
+
+############# Figure 9 D ####################################
+
+## import the data
+res=read.csv('Figure_9_D.csv')
+
+## add relative intensity column
+res['relative.intensity']=res$dHex_Intensity/res$total_Intensity
+
+## plot the results IL-1 beta
+temp=cor.test(res$relative.intensity, log2(as.numeric(res$IL1.beta)))
+coefficient=as.vector(temp[[4]])
+coefficient=round(coefficient,digits = 2)
+p=temp[[3]]
+p=round(p, digits = 4)
+ggplot(res, aes(relative.intensity*100, log2(as.numeric(IL1.beta))))+
+  geom_point()+
+  labs(x='Glycan proportion %', y='log2(IL-1 beta)',
+       subtitle = paste('coefficient = ',coefficient,', ', 'p = ',p,sep=''))+
+  geom_smooth(method=lm)+
+  theme( plot.subtitle = element_text(hjust = 0.5, face = 'italic'))
+
+## plot the results IL-18
+temp=cor.test(res$relative.intensity, log2(as.numeric(res$IL.18)))
+coefficient=as.vector(temp[[4]])
+coefficient=round(coefficient,digits = 2)
+p=temp[[3]]
+p=round(p, digits = 4)
+ggplot(res, aes(relative.intensity*100, log2(as.numeric(IL.18))))+
+  geom_point()+
+  labs(x='Glycan proportion %', y='log2(IL-1 18)',
+       subtitle = paste('coefficient = ',coefficient,', ', 'p = ',p,sep=''))+
+  geom_smooth(method=lm)+
+  theme( plot.subtitle = element_text(hjust = 0.5, face = 'italic'))
